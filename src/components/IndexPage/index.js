8df390c1987a7acc49fca8af3ef3e281e91802b8
Ugo Cavalcanti <ugocavalcanti@gmail.com>
2021-01-26 00:28:11 -0300

    corrigir metadados

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -3,11 +3,15 @@ import Head from 'next/head'
 function IndexPage() {
   return (
       <Head>
-        <title>Java Quiz</title>
-        <meta property="og:image" content="https://sempreupdate.com.br/wp-content/uploads/2018/12/como-instalar-o-oracle-java-11-no-ubuntu-linux-mint-ou-debian.jpg" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="800" /> /** PIXELS **/
-        <meta property="og:image:height" content="600" /> /** PIXELS **/    
+            <title>Você sabe tudo sobre Java?</title>
+            <meta name="title" content="Você sabe tudo sobre Java?" />
+            <meta name="description" content="" />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://javaquiz-nextjs.ugocavalcanti.vercel.app/" />
+            <meta property="og:title" content="Você sabe tudo sobre Java?" />
+            <meta property="og:description" content="" />
+            <meta property="og:image" content="https://sempreupdate.com.br/wp-content/uploads/2018/12/como-instalar-o-oracle-java-11-no-ubuntu-linux-mint-ou-debian.jpg" />
       </Head>
   )
 }
